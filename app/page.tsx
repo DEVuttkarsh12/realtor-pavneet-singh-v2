b038258ddd8d9ae70f4120108f4476a3f15dc8ca
DEVuttkarsh12 <uttkarsh.bhardwaj12@gmail.com>
2026-09-04 16:22:31 +0530

    Improve homepage search metadata

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,12 @@
+import type { Metadata } from "next";
 import HomeExperience from "./components/HomeExperience";
+
+export const metadata: Metadata = {
+  title:
+    "New Apartments for Rent in Upper Hammonds Plains | Lifestyle Enclave",
+  description:
+    "Brand-new 1-bedroom + den and 2-bedroom + den apartments from $1,750. Parking, water, gym and in-suite laundry included. Book an October 2026 tour.",
+};
 
 export default function Home() {
   return <HomeExperience />;
